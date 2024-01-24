ed6246e010a5eb09ca8e5a4093b013f118f97a59
ArchanaSharikalHarinarayanan <archana.s.h@saveetha.ac.in>
2024-01-24 15:06:19 +0530

    First commit

---
+++ Assets/cubeProg.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class cubeProg : MonoBehaviour
+{   
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+       
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
